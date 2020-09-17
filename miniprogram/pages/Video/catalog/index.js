12d7d018cf01a4563e3a6583f6c6ccb4efd91b02
kxy <1411533738@qq.com>
2020-09-17 15:07:55 +0800

    add some pages

---
+++ miniprogram/pages/Video/catalog/index.js
@@ -0,0 +1,23 @@
+// pages/Video/catalog/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
--- miniprogram/pages/Video/video.js
+++ miniprogram/pages/Video/video.js
@@ -1,66 +1,111 @@
 // miniprogram/pages/Video/video.js
+import polyv from '../../utils/polyv.js';
+
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        src: '',
+        current: '1',
+        // pane
 
-  },
+    },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
 
-  },
+    },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
+        let vid = '3828390191faee6d7d4f9b9acb8b646d_3';
 
-  },
+        /*获取视频数据*/
+        let obj = {
+            vid: vid,
+            viewerInfo: {
+                viewerId: '38770077709', // 播放观看日志学员ID
+                viewerName: 'polyv' // 播放观看日志学员昵称
+            },
+            callback: videoInfo => {
+                if (videoInfo.type === 'error') {
+                    console.log('videoInfo', videoInfo);
+                    return;
+                }
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+                this.setData({
+                    src: videoInfo.src[0],
+                });
+            }
+        };
 
-  },
+        this.player = polyv.getVideo(obj);
+        // setInterval(() => {
+        //   const data = this.player.j2s_realPlayStatus();
+        //   console.info(data, '---data---');
+        // }, 3000);
+    },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
 
-  },
+    },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
 
-  },
+    },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
+        this.player.destroy();
 
-  },
+    },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
 
-  },
+    },
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
 
-  }
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
+    },
+
+    timeupdate: function (e) {
+        this.player.timeUpdate(e);
+    },
+
+    handleChange({detail}) {
+        const that = this
+        that.setData({
+            current: detail.key
+        });
+        that.setData({})
+    },
+
+
 })
